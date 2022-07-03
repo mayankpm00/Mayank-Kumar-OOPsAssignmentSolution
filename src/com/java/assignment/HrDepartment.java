737008e79f13b9f79bdc6379f1d22a31de46d754
Mayank Kumar <mayanksingh2090@gmail.com>
2022-07-03 17:36:30 +0530

    first commit

---
+++ src/com/java/assignment/HrDepartment.java
@@ -0,0 +1,16 @@
+package com.java.assignment;
+
+public class HrDepartment extends SuperDepartment{
+	public String departmentName(){
+		return "Hr Department";
+	}
+	public String getTodaysWork(){
+		return "Fill today’s worksheet and mark your attendance";
+	}
+	public String getWorkDeadline(){
+		return "Complete by EOD";
+	}
+	public String doActivity(){
+		return "team Lunch";
+	}
+}
